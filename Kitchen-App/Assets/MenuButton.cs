9e6a9f4fef32eb82312b8a27d64a1c7eac522f6d
ronAvot <ron952@gmail.com>
2017-08-02 21:19:42 +0300

    Kitchen AR app

---
+++ Kitchen-App/Assets/MenuButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MenuButton : MonoBehaviour {
+
+    public GameObject Model;
+    bool isHide = true;
+        public Button yourButton;
+
+    
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+    
+    
+
+    void hide()
+    {
+        if (isHide)
+        {
+            Model.SetActive(true);
+            isHide = false;
+        }
+
+        else
+        {
+            Model.SetActive(false);
+            isHide = true;
+        }
+    }
+
+    
+    
+    void TaskOnClick()
+    {
+
+        hide();
+    }
+
+    
+}
